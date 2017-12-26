e16a7f81c25d237c8b9fce0f27bef2c1810d7b0f
tidus30691nexus5 <tidus30691.nexus5@gmail.com>
2017-12-26 17:42:53 +0700

    disable keepalive feature
    disable keep-alive feature

--- src/centralserver.c
+++ src/centralserver.c
@@ -348,26 +348,7 @@ connect_auth_server()
 void
 decrease_authserv_fd_ref()
 {
-	s_config *config = config_get_config();
-    t_auth_serv *auth_server = NULL;
-	
-	LOCK_CONFIG();
-
-	for (auth_server = config->auth_servers; auth_server; auth_server = auth_server->next) {
-        if (auth_server->authserv_fd > 0) {
-			auth_server->authserv_fd_ref -= 1;
-			if (auth_server->authserv_fd_ref == 0) {
-				debug(LOG_INFO, "authserv_fd_ref is 0, but not close this connection");
-			} else if (auth_server->authserv_fd_ref < 0) {
-				debug(LOG_ERR, "Impossible, authserv_fd_ref is %d", auth_server->authserv_fd_ref);
-				close(auth_server->authserv_fd);
-				auth_server->authserv_fd = -1;
-				auth_server->authserv_fd_ref = 0;
-			}
-		}
-    }
-	
-	UNLOCK_CONFIG();
+	close_auth_server();
 }
 
 void
@@ -386,13 +367,9 @@ _close_auth_server()
 	
 	for (auth_server = config->auth_servers; auth_server; auth_server = auth_server->next) {
         if (auth_server->authserv_fd > 0) {
-			auth_server->authserv_fd_ref -= 1;
-			if (auth_server->authserv_fd_ref <= 0) {
-				debug(LOG_DEBUG, "authserv_fd_ref is %d, close this connection", auth_server->authserv_fd_ref);
-				close(auth_server->authserv_fd);
-				auth_server->authserv_fd = -1;
-				auth_server->authserv_fd_ref = 0;
-			} 
+			debug(LOG_DEBUG, "authserv_fd_ref is %d, close this connection", auth_server->authserv_fd_ref);
+ 			close(auth_server->authserv_fd);
+ 			auth_server->authserv_fd = -1;
 		}
     }
 }
@@ -422,20 +399,7 @@ _connect_auth_server(int level) {
 		return -1;
 	}
 	
-	auth_server = config->auth_servers;
-	if (auth_server->authserv_fd > 0) {
-		if (is_socket_valid(auth_server->authserv_fd)) {
-			debug(LOG_INFO, "Use keep-alive http connection, authserv_fd_ref is %d", auth_server->authserv_fd_ref);
-			auth_server->authserv_fd_ref++;
-			return auth_server->authserv_fd;
-		} else {
-			debug(LOG_INFO, "Server has closed this connection, initialize it");
-			close(auth_server->authserv_fd);
-			auth_server->authserv_fd = -1;
-			auth_server->authserv_fd_ref = 0;
-			return _connect_auth_server(level);
-		}
-	}
+	
 	
     /* XXX level starts out at 0 and gets incremented by every iterations. */
     level++;
@@ -462,7 +426,8 @@ _connect_auth_server(int level) {
      */
 	auth_server = config->auth_servers;
     hostname = auth_server->authserv_hostname;
-    debug(LOG_DEBUG, "Level %d: Resolving auth server [%s]", level, hostname);
+    if (NULL == auth_server->last_ip) {
+	debug(LOG_DEBUG, "Level %d: Resolving auth server [%s]", level, hostname);
     h_addr = wd_gethostbyname(hostname);
     if (!h_addr) {
         /*
@@ -504,7 +469,10 @@ _connect_auth_server(int level) {
              */
             free(ip);
         }
+		free(h_addr);
+	}
 
+	}	
         /*
          * Connect to it
          */
@@ -513,9 +481,8 @@ _connect_auth_server(int level) {
 
         their_addr.sin_port = port;
         their_addr.sin_family = AF_INET;
-        their_addr.sin_addr = *h_addr;
-        memset(&(their_addr.sin_zero), '\0', sizeof(their_addr.sin_zero));
-        free(h_addr);
+		inet_aton(auth_server->last_ip, &their_addr.sin_addr);
+        memset(&(their_addr.sin_zero), '\0', sizeof(their_addr.sin_zero)); 
 
         if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
             debug(LOG_ERR, "Level %d: Failed to create a new SOCK_STREAM socket: %s", strerror(errno));
@@ -526,8 +493,7 @@ _connect_auth_server(int level) {
 							 auth_server->authserv_connect_timeout);
 		if (res == 0) {
 			// connect successly
-			auth_server->authserv_fd = sockfd;
-			auth_server->authserv_fd_ref++;
+			
 			return sockfd;
 		} else {
 			debug(LOG_INFO,
@@ -537,8 +503,8 @@ _connect_auth_server(int level) {
 			mark_auth_server_bad(auth_server);
 			return _connect_auth_server(level); /* Yay recursion! */
 		}
-    }
-}
+  }
+
 
 // 0, failure; 1, success
 static int
